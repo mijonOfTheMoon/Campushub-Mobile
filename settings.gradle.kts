3cad047f54f5dd004a7ea28f40f60547d9ba4d2f
Hasbi Mizan <hasbimizan@student.ub.ac.id>
2025-06-20 12:40:54 +0700

    Refactor: Remove unused layout files and resources
    - Deleted activity_myevent.xml, activity_terdaftar.xml, item_event.xml, and associated drawable resources.
    - Removed launcher icons and related XML files from mipmap directories.
    - Cleaned up themes, colors, strings, and XML backup rules.
    - Updated build.gradle.kts to include Google services plugin.
    - Updated dependencies in libs.versions.toml for improved library versions.
    - Added JitPack repository to settings.gradle.kts for dependency resolution.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,9 +12,11 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
+        maven { url = uri("https://jitpack.io") }
         mavenCentral()
     }
 }
